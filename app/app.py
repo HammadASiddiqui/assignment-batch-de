ce3351d23daed7c6e856feaba4bfb02c532eebe9
HammadASiddiqui <hammadahmed789@gmail.com>
2022-07-21 23:08:03 +0500

    adding the comments

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 
 # intialize connection parameters for postgres database
 def init_db(db_creds):
+    #extract credentials from dictionary for connecting DB
     db=None
     db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_creds['db_user'], db_creds['db_pass'], db_creds['db_host'], db_creds['db_port'], db_creds['db_name'])
     try:
@@ -16,10 +17,12 @@ def init_db(db_creds):
     finally:
         return db
 
-#insert user's data
+#insert data for the exception in bulkload(row by row)
 def insert_into_table(data_array, table_key):
     for record in data_array:
+        #Sqlalchemy ORM based insert function
         ingest = table_key.insert()
+        #execution of insertion statement
         try:
             ingest.execute(record)
         except Exception as e:
@@ -30,8 +33,10 @@ def insert_into_table(data_array, table_key):
 def bulkload_insertion_into_table(data_array, table_key):
     print("______________ batch ingestion start _______________________")
     
+    #Sqlalchemy ORM based insert function
     ingest = table_key.insert()
     
+    #execution of bulkload insertion statement on whole data array of objects
     try:
         ingest.execute(data_array)
     except Exception as e:
@@ -49,6 +54,8 @@ def process_page(page_results, tweets, users):
     batch_tweets=[]
     batch_users=[]
     for i, tweet in enumerate(page_results):
+        
+        #creating dictionary contaning info from the extracted tweets JSON 
         tweet_dict = {'tweet_id': str(tweet._json['id_str']),
         'created_at': tweet._json['created_at'],
         'tweet_text': str(tweet._json['text']),
@@ -57,18 +64,23 @@ def process_page(page_results, tweets, users):
         'retweet_count': tweet._json['retweet_count'],
         'raw_json':str(tweet._json),
         'user_id': str(tweet._json['user']['id_str'])}
+        
+        #appending the tweets object data array
         batch_tweets.append(tweet_dict)
         
         
+        #creating dictionary contaning info from the extracted tweets JSON for users
         user_dict= {
         'user_id': str(tweet._json['user']['id_str']),
         'user_name': str(tweet._json['user']['name']),
         'user_url': str(tweet._json['user']['url']),}
 
+        #appending the user object data array
         batch_users.append(user_dict)
         
 
 
+    #bulkload ingestion for the whole batch of 20 tweets of a page
     bulkload_insertion_into_table( batch_users, users)
     bulkload_insertion_into_table( batch_tweets, tweets)
     
@@ -78,23 +90,28 @@ def process_page(page_results, tweets, users):
 if __name__ == '__main__':
     print('application started')
 
+    #reading DB credential file for extracting credentials
     try:
         with open('db_creds.yaml') as f:
             db_creds_dict = yaml.load(f, Loader=SafeLoader)
     except:
         print("db_creds.yaml file not found")
     
+    #create connection to DB
     db = init_db(db_creds_dict)
 
     
+    #reading api auth file for extracting api keys
     try:
         with open('api_auth.yaml') as f:
             keys = yaml.load(f, Loader=SafeLoader)
     except:
         print("api_auth.yaml not found")
     
+    #sqlalchemy function to relate metadata info from db with program based ORM structures
     metadata = MetaData(db)
 
+    #structural defination for tweets table fro ORM based ingestion
     tweets = Table('tweets', metadata,
         Column('tweet_id', String, primary_key=True),
         Column('created_at', TIMESTAMP, primary_key=True),
@@ -106,28 +123,25 @@ if __name__ == '__main__':
         Column('user_id', String),
     )
 
+    #structural defination for user table fro ORM based ingestion
     users = Table('users', metadata,
         Column('user_id', String, primary_key=True),
         Column('user_name', String),
         Column('user_url', String),
     )
     
-    
+    #creating auth keys from the read dictionary
     OAUTH_KEYS = {'consumer_key': keys['consumer_key'], 'consumer_secret': keys['consumer_secret'],
                   'access_token_key': keys['access_token_key'], 'access_token_secret': keys['access_token_secret']}
     auth = tweepy.OAuthHandler(
         OAUTH_KEYS['consumer_key'], OAUTH_KEYS['consumer_secret'])
 
+    #setting auth for API calling
     api = tweepy.API(auth)
 
     keyword = "ikea"
 
-    # for i, page in enumerate(tweepy.Cursor(api.search_tweets, q=keyword, count=20, lang='en').pages(50)):
-    #     process_page(page, tweets, users)
-    #     print('-------------', i, '--------------')
-
-    
-
-    for i, page in enumerate(tweepy.Cursor(api.search_tweets, q=keyword,until=datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d'), lang='en').pages()):
+    #iterate over the API results and pagenate them in size of 20 tweets per page extracting the week's tweet
+    for i, page in enumerate(tweepy.Cursor(api.search_tweets, q=keyword,until=datetime.strftime(datetime.now(), '%Y-%m-%d'), lang='en').pages()):
         process_page(page, tweets, users)
         print('-------------', i, '--------------')
